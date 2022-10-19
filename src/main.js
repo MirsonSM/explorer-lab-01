8c4531f4e88c05124f5a5be88fe9ee3d12fc9862
Mailson Moreira <mailsoneng@gmail.com>
2022-10-18 22:48:50 -0300

    Alterações segunda aula

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import "./css/index.css"
+import IMask from "imask"
 
 
 const ccBgColor01 = document.querySelector(".cc-bg svg > g g:nth-child(1) path")
 const ccBGColor02 = document.querySelector('.cc-bg svg > g g:nth-child(2) path')
 const cclogo = document.querySelector('.cc-logo span:nth-child(2) img')
+
+
+
 
 function setCardType(type) {
   const colors = {
@@ -17,5 +21,39 @@ function setCardType(type) {
 }
 
 setCardType('mastercard')
+
+//security code
+
+const securityCode = document.querySelector("#security-code")
+const securyCodePattern = {
+  mask:'0000'
+}
+
+const securityCodeMasked = IMask(securityCode, securyCodePattern)
+
+const expirationDate = document.querySelector('#expiration-date')
+const expeirationDatePattern = {
+  mask:'MM{/}YY',
+  blocks: { //usado para criar o range - Nesse caso para mês e ano.
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2), // String está transformando o ano e texto para pegtar os dois ultimos dígitos com o slice(2)
+      to: String(new Date().getFullYear()+10 ).slice(2)
+    },
+    MM: {
+    mask: IMask.MaskedRange,
+    from: 1,
+    to: 12
+    }
+  } 
+}
+
+
+
+const expirantioDateMasked = IMask(expirationDate, expeirationDatePattern)
+
+
+
+
 
 
